3eb6c42de5724266aee57a8bd3d06d4c5890477e
hasanmd91 <hasanmd1991@hotmail.com>
2022-11-05 23:29:09 +0200

    buble sort algo

---
+++ sort algo/bubblesort.js
@@ -0,0 +1,20 @@
+function bubblesort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+}
+
+const arr = [8, 20, -2, -8, 4, -6];
+
+bubblesort(arr);
+
+console.log(arr);
